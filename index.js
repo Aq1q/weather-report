8d3025440f5009a42680f3919e38c438e4bd08c4
Aq1q <micheu334@gmail.com>
2023-08-19 13:54:40 +0200

    feat: hit wearher api

---
+++ index.js
@@ -0,0 +1,10 @@
+const search = document.getElementById('location');
+console.log(search);
+async function getData() {
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=1e2ff4f6408445b3821114513231908 &q=${search.value}&aqi=yes`, {
+        mode: 'cors'
+    }
+    )
+    const json = await response.json();
+    console.log(json);
+}
