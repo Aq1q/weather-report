90ebab31ee2949491a0d55ee6e0ef194edc6deaa
Aq1q <micheu334@gmail.com>
2023-08-19 14:12:44 +0200

    feat: process weather data

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const search = document.getElementById('location');
+
+async function weatherApp() {
+    let json = await getData();
+    let data = {
+        country: json.location.country,
+        time: json.location.localtime,
+        city: json.location.name,
+        feels_like: json.current.feelslike_c,
+        gust: json.current.gust_kph,
+        is_day: json.current.is_day,
+        temp: json.current.temp_c,
+        wind_speed: json.current.wind_kph,
+        wind_direction: json.current.wind_dir,
+        condition: json.condition.text,
+        humidity: json.current.humidity
+    }
+    console.log(data);
+}
+
+
 async function getData() {
     const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=1e2ff4f6408445b3821114513231908 &q=${search.value}&aqi=yes`, {
         mode: 'cors'
     }
     )
-    const json = await response.json();
-    console.log(json);
+    let json = await response.json();
+    return json;
 }
